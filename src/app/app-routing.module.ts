e86b9012f6ac33ae2af61609bd2a73652d73031c
mangojuicedeli <mgkang0317@gmail.com>
2020-06-24 16:08:28 +0900

    login html, css, ts 기본 설정

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
-
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
-  { path: 'sign-up', component: SignUpComponent }
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
