61e4c91debfeb556a22b980940ded54ea7613aa3
mangojuicedeli <mgkang0317@gmail.com>
2020-06-26 14:07:11 +0900

    ngx-toastr을 이용한 signup/login 기능 ui 개선 및 리팩토링

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -12,20 +12,22 @@ import { map } from 'rxjs/operators';
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient, private localStorage: LocalStorageService) {
-  }
+  constructor(private httpClient: HttpClient, private localStorage: LocalStorageService) { }
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
+
     return this.httpClient.post('http://localhost:8080/api/auth/signup', signupRequestPayload, { responseType: 'text' });
   }
 
   login(loginRequestPayload: LoginRequestPayload): Observable<boolean> {
+
     return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/login', loginRequestPayload)
       .pipe(map(data => {
         this.localStorage.store('authenticationToken', data.authenticationToken);
         this.localStorage.store('username', data.username);
         this.localStorage.store('refreshToken', data.refreshToken);
         this.localStorage.store('expiresAt', data.expiresAt);
+        
         return true;
       }));
   }
