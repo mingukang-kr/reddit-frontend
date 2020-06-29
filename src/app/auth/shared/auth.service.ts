19e991102e837a1108794e2a1337e923034bd703
mangojuicedeli <mgkang0317@gmail.com>
2020-06-29 17:26:32 +0900

    logout 구현

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { SignupRequestPayload } from '../sign-up/signup-request.payload';
+import { SignupRequestPayload } from '../signup/singup-request.payload';
 import { Observable, throwError } from 'rxjs';
 import { LocalStorageService } from 'ngx-webstorage';
 import { LoginRequestPayload } from '../login/login-request.payload';
